14c2ce4fbe78b14dc4b0831eb6a692296d190bf5
ghozalihadi <ghozali.hadi@gmail.com>
2025-04-11 05:36:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ghozalihadi.github.io" 
---
+++ script.js
@@ -0,0 +1,5 @@
+// Smooth scroll handled by CSS already
+// Optional scroll-based animation could go here
+
+// Future: scroll reveal, theme toggling, etc.
+console.log("Page loaded! Add more interactivity here if needed.");
